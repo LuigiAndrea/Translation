009d82735fbc8ca5fbda16805abaec8e3048343c
Luigi Andrea D'Aversa <luigiandrea@gmail.com>
2017-03-18 02:00:06 -0400

    e2e test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,15 +1,17 @@
-import { browser, element, by } from 'protractor';
+import { AppPage } from './app.po';
 
 describe('QuickStart E2E Tests', function () {
 
   let expectedMsg = 'Hello i18n!';
+  let p: AppPage;
 
-  beforeEach(function () {
-    browser.get('');
+  beforeEach(() => {
+    p = new AppPage();
+    p.navigate();
   });
 
   it('should display: ' + expectedMsg, function () {
-    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+    expect(p.getHeaderText()).toEqual(expectedMsg);
   });
 
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getHeaderText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('h1')).getText();
   }
 }
